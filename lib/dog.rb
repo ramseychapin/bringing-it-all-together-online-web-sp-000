63b896c4a97902edbc6ee857c5baa0c6951611d6
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-11 21:59:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,11 +20,7 @@ class Dog
 
   def self.drop_table
     sql =  <<-SQL
-      DROP TABLE dogs (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        breed TEXT
-        )
+      DROP TABLE dogs;
         SQL
     DB[:conn].execute(sql)
   end
