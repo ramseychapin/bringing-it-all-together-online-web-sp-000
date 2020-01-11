df0deab34de557aa939249d7274c22769dca0712
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-11 22:52:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,7 @@ class Dog
      SQL
 
      given_dog = DB[:conn].execute(sql, name, breed)[0]
-     if !given_dog.empty?
+     if given_dog
        new_dog = Dog.new(id: result[0], name: result[1], breed: result[2])
        new_dog
     else
