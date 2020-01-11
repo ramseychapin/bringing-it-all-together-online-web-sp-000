9db030b184962e7230424ccdd2bd741f752379af
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-11 22:53:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,7 +65,7 @@ class Dog
 
      given_dog = DB[:conn].execute(sql, name, breed)[0]
      if given_dog
-       Dog.new(id: result[0], name: result[1], breed: result[2])
+       Dog.new(id: given_dog[0], name: result[1], breed: result[2])
     else
       new_dog = self.create(name, breed)
     end
