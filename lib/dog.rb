2c2f113df105ae690c757b05d98f6813e8d7947c
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-11 22:50:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,10 +67,10 @@ class Dog
      binding.pry
      if given_dog
        new_dog = Dog.new(id: result[0], name: result[1], breed: result[2])
+       new_dog
     else
       new_dog = self.create(name, breed)
     end
-    new_dog
   end
 
   def self.find_by_name(name)
