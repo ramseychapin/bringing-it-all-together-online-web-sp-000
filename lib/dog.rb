ae03cf1fe83f8bc1669bff676d04be46bf2e097c
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-11 22:31:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ class Dog
   end
 
   def update
-    sql = "UPDATE dogs SET name = ?, grade = ? WHERE id = ?"
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
