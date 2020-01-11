f1730a5f1de51562922a59489f8a139249530cf9
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-11 22:16:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,10 +46,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    Dog.new(id,name,breed)
+    self new(id: row[0],name: row[1],breed: row[2])
   end
 
   def self.find_by_id
