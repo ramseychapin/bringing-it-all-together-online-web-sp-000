a7d69c43925dfa25edd3dc7872296623561cccda
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-11 21:57:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,6 +18,17 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
 
 
 end
